07aafa503c2a705489cc491c0c9f9f4d0db8d4dd
lamiru <lamiru@gmail.com>
2016-05-17 15:25:15 +0900

    add blog app

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls')),
     url(r'^photos/', include('photos.urls')),
 ]
