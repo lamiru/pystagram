36096957712228b0717023cf97159d7dc9ef45ce
lamiru <lamiru@gmail.com>
2016-05-17 16:56:18 +0900

    edit blog index view / add blog detail view

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'blog.views.index'),
+    url(r'^(?P<pk>\d+)$', 'blog.views.detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 def index(request):
+    count = request.session.get('index_page_count', 0) + 1
+    request.session['index_page_count'] = count
+
+    post_list = Post.objects.all()
+
     return render(request, 'blog/index.html', {
+        'count': count,
+        'post_list': post_list,
+    })
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/detail.html', {
+        'post': post,
     })
