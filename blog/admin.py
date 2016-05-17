c8114ccc691b039743a726d9cb7e616495daa5e5
lamiru <lamiru@gmail.com>
2016-05-17 18:15:17 +0900

    edit blog PostAdmin / add blog CategoryAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from blog.models import Category, Post, Comment, Tag
 
-admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'category', 'title')
+    list_display = ('id', 'uuid', 'category', 'title')
+    search_fields = ('title', 'content')
 
 admin.site.register(Post, PostAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
