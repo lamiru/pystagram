1f648b462c702fc900cf23c617d1c078a8473d37
lamiru <lamiru@gmail.com>
2016-05-17 15:02:46 +0900

    add photos detail view

--- photos/views.py
+++ photos/views.py
@@ -6,3 +6,9 @@ def index(request):
     return render(request, 'photos/index.html', {
         'post_list': post_list,
     })
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'photos/detail.html', {
+        'post': post,
+    })
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^photos/$', 'photos.views.index'),
+    url(r'^photos/(?P<pk>\d+)/$', 'photos.views.detail'),
 ]
