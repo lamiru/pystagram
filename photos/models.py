498b10b203ec00922d958dc2954daf04fb071436
lamiru <lamiru@gmail.com>
2016-05-17 14:39:33 +0900

    add __str__ method to photos Post model

--- photos/models.py
+++ photos/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
