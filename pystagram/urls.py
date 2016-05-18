5159e23d7d5b0e43963b576cfea52580e3430c69
lamiru <lamiru@gmail.com>
2016-05-18 20:13:18 +0900

    add accounts app / add login and logout features to accounts app

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^photos/', include('photos.urls')),
 ]
