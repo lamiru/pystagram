5657f37cbbba6392a5d0a979a9057aec2d880dd0
lamiru <lamiru@gmail.com>
2016-05-18 21:50:07 +0900

    edit message tags in settings.py

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -107,3 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
