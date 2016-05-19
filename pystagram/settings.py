f844c5fd216814f3a94d900e1b475c587c2ff0d7
lamiru <lamiru@gmail.com>
2016-05-19 12:28:54 +0900

    add jquery file / change static files directory

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -107,6 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'pystagram', 'static'),
+)
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
