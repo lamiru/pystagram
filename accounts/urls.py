f22e301bbf7b884ddc882341b041be9f479a0d72
lamiru <lamiru@gmail.com>
2016-05-19 10:14:11 +0900

    add login, logout and profile_detail urls to layout template

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^signup/$', 'accounts.views.signup'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'django.contrib.auth.views.login'}),
     url(r'^profile/$', 'accounts.views.profile_detail'),
     url(r'^profile/edit$', 'accounts.views.profile_edit'),
 ]
