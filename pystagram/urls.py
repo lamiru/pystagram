ef14eb90194c1bf63cad0b1a059250ddb016e5b8
lamiru <lamiru@gmail.com>
2016-05-24 19:29:03 +0900

    redirect root url to blog index url

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(pattern_name='blog:index')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
